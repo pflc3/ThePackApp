342416286a8d257e32b55a42bde53113b6d7a3fe
Estuardo Alexis Lopez Letona <estulpzlet@gmail.com>
2024-08-03 13:55:41 -0400

    Initial Commit

---
+++ ThePackApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThePackApp
+//
+//  Created by Estuardo Lopez Letona on 8/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThePackApp/ThePackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThePackAppApp.swift
+//  ThePackApp
+//
+//  Created by Estuardo Lopez Letona on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ThePackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
