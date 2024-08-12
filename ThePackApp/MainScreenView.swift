a3cd55b1342bc9960cf8f8f9135d8df4b9add3c3
Nathaniel <Gcodena2024@gmail.com>
2024-08-12 13:46:21 -0400

    commit

---
+++ ThePackApp/MainScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreenView.swift
+//  ThePackApp
+//
+//  Created by Gcode on 8/12/24.
+//
+
+import SwiftUI
+
+struct MainScreenView: View {
+    var body: some View {
+        Text("Main Screen!")
+    }
+}
+
+#Preview {
+    MainScreenView()
+}
