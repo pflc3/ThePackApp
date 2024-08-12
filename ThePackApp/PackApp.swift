3a86d1a82d26dcf9fd15d2fac8c078527ce6bedc
Estuardo Alexis Lopez Letona <estulpzlet@gmail.com>
2024-08-12 11:02:13 -0400

    Changed file

---
+++ ThePackApp/PackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackApp.swift
+//  ThePackApp
+//
+//  Created by Estuardo Lopez Letona on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
