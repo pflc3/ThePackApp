37fa9c0096126855d2d9393c487566dc1bb397b2
Bradley <gcodebl.2024@gmail.com>
2024-08-13 12:07:53 -0400

    created a new branch and file for the dog list view

---
+++ ThePackApp/DogListView.swift
@@ -0,0 +1,18 @@
+//
+//  DogListView.swift
+//  ThePackApp
+//
+//  Created by Gcode on 8/13/24.
+//
+
+import SwiftUI
+
+struct DogListView: View {
+    var body: some View {
+        Text("Dog List View!")
+    }
+}
+
+#Preview {
+    DogListView()
+}
