24bf5419b1495822bb767ba914ca65d795ff1ef0
Fatoumata <gocdefj2024@gmail.com>
2024-08-20 11:59:32 -0400

    i added my comment section

--- ThePackApp/PostBoardFeature/PostDetailView.swift
+++ ThePackApp/PostBoardFeature/PostDetailView.swift
@@ -8,14 +8,63 @@ import SwiftUI
 struct PostDetailView: View {
     var body: some View {
         VStack {
-            customTitle(title: "Title", w: 360, h: 65)
+            customTitle(title: "Post Title", w: 360, h: 65)
+            HStack {
+                Image(systemName: "camera.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.blue)
+                .padding(.leading, 10)
+              Spacer()
+                Text("category")
+                    .foregroundColor(.blue)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        
+            Spacer()
+            
+            Image(systemName: "camera.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50, height: 40)
+                .foregroundColor(.white)
+                .padding(.leading, 10)
+            
+            @State var Post: String = ""
+
+            
+            
+      
+            Text("dksjbver wepiofjrnew wdieorh pweiorhjf l2ekfn wdeliwnr dksjbver wepiofjrnew wdieorh pweiorhjf l2ekfn wdeliwnr dksjbver wepiofjrnew wdieorh pweiorhjf l2ekfn wdeliwnr dksjbver wepiofjrnew wdieorh pweiorhjf l2ekfn wdeliwnr dksjbver wepiofjrnew wdieorh pweiorhjf l2ekfn wdeliwnr dksjbver wepiofjrnew wdieorh pweiorhjf l2ekfn wdeliwnr dksjbver wepiofjrnew wdieorh pweiorhjf l2ekfn wdeliwnr dksjbver wepiofjrnew wdieorh pweiorhjf l2ekfn wdeliwnr")
             
             Spacer()
+            Spacer()
+            
+            Text("dksjbver wepiofjrnew wdieorh pweiorhjf l2ekfn wdeliwnr dksjbver wepiofjrnew wdieorh pweiorhjf l2ekfn wdeliwnr dksjbver wepiofjrnew wdieorh pweiorhjf l2ekfn wdeliwnr dksjbver wepiofjrnew wdieorh pweiorhjf l2ekfn ")
+            
+            HStack {
+                Image ("Image")
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(70)
+                    .frame(width: 50, height: 70)
+                    .foregroundColor(.white)
+                    .padding(.leading, 10)
+                @State var firstName: String = ""
+                let fontSize: CGFloat = 20
+                
+                    // Question 1: First Name
+                    Text("")
+                        .font(.system(size: fontSize))
+                        .fontWeight(.medium)
+                    TextField("Comment your thoughts", text: $firstName)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
         }
-        .padding()
-        .background(blueGradient())
     }
 }
+    
 
 #Preview {
     PostDetailView()
