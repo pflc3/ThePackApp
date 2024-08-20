a4c6827c653124c83570633a508392c335ccc90f
Bradley <gcodebl.2024@gmail.com>
2024-08-20 14:01:56 -0400

    starting json

--- ThePackApp/PostBoardFeature/Views/PostRow.swift
+++ ThePackApp/PostBoardFeature/Views/PostRow.swift
@@ -7,46 +7,46 @@ import SwiftUI
 
 struct PostRow: View {
     var body: some View {
-        NavigationLink(destination: PostDetailView()) {
-            HStack {
-                Image(systemName: "camera.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(.white)
-                    .padding(.leading, 10)
-                
-                
-                Text("Post Title")
-                    .font(.title3)
-                    .bold()
-                    .foregroundColor(.white)
-                    .padding(.leading, 10)
-                
-                Spacer()
-                
-                Text("Category")
-                    .font(.headline)
-                    .fontWeight(.medium)
-                    .foregroundColor(.white)
-                    .padding(.trailing, 10)
-                
-                
-            }
-            .padding()
-            .background(
-                LinearGradient(
-                    gradient: Gradient(colors: [Color.blue.opacity(0.7), Color.blue]),
-                    startPoint: .leading,
-                    endPoint: .trailing
-                )
-            )
-            .cornerRadius(15)
-            .shadow(color: Color.blue.opacity(0.5), radius: 5, x: 0, y: 2)
-            .scaleEffect(1)
-            .padding(.horizontal)
+        HStack {
+            Image(systemName: "camera.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 35, height: 35)
+                .foregroundColor(.white)
+                .padding(.leading, 10)
+            
+            Text("Post Title")
+                .font(.title)
+                .fontWeight(.medium)
+                .foregroundColor(.white)
+                .padding(.leading, 10)
+            
+            Spacer()
+            
+            Text("Category")
+                .font(.title3)
+                .fontWeight(.medium)
+                .foregroundColor(.white)
+                .padding(.trailing, 10)
         }
+        .padding(.vertical, 12)
+        .background(
+            LinearGradient(
+                gradient: Gradient(colors: [Color.blue.opacity(0.7), Color.blue]),
+                startPoint: .leading,
+                endPoint: .trailing
+            )
+        )
+        .cornerRadius(15)
+        .shadow(color: Color.blue.opacity(0.5), radius: 5, x: 0, y: 2)
+        .padding(.horizontal)
     }
 }
 
+#Preview {
+    Group {
+        PostRow()
+        PostRow()
+    }
+}
 
