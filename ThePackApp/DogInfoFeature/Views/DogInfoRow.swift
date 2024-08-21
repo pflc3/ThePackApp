e7dd4262ad23764622957153f1224afd963d0fe5
Fatoumata <gocdefj2024@gmail.com>
2024-08-21 12:03:15 -0400

    bark board

--- ThePackApp/DogInfoFeature/Views/DogInfoRow.swift
+++ ThePackApp/DogInfoFeature/Views/DogInfoRow.swift
@@ -20,7 +20,7 @@ struct DogInfoRow: View {
                 .fontWeight(.medium)
             
             Spacer()
-
+            
             Text(dogInfoVar.lifeStage)
                 .font(.headline)
                 .fontWeight(.light)
