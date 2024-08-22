3e78a8fabcdcd64a4fb1128519ab130afb0e888b
Fatoumata <gocdefj2024@gmail.com>
2024-08-22 10:58:09 -0400

    we edited the comment section

--- ThePackApp/PostBoardFeature/Views/PostDetailView.swift
+++ ThePackApp/PostBoardFeature/Views/PostDetailView.swift
@@ -13,21 +13,51 @@ struct PostDetailView: View {
             // Camera icon and category
             HStack {
                 Image(systemName: "camera.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 25, height: 25)
+                    .foregroundColor(.white)
                 Text(postVar.category)
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
             }
+            .padding(.horizontal, 10)
+            .padding(.vertical, 5)
+            .background(Color.blue.opacity(0.9))
+            .shadow(radius: 5)
+            .cornerRadius(15)
             
             // Title
             Text(postVar.title)
+                .font(.title)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .center)
+                .foregroundColor(.white)
+                .padding(.all, 10)
+                .background(customRectangle(colorVar:. blue))
+                .shadow(radius: 20)
             
             Spacer().frame(height: 25)
             
             // Description
             Text(postVar.description)
+                .font(.system(size: 20))
+                .fontWeight(.medium)
+                .padding()
+                .background(customRectangle())
             
             Spacer().frame(height: 25)
             
             // Comment Section Title
             Text("Comments")
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .padding(.horizontal, 8)
+                .padding(.vertical, 4)
+                .background(customRectangle(colorVar: .blue))
+                
             
             Spacer().frame(height: 10)
             
